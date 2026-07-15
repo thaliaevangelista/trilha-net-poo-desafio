97927ed43c0bb737a2eec4a891de2b2afd630317
Thalia Evangelista <thalia.soares.e27@gmail.com>
2026-07-15 20:28:30 -0300

    Desafio POO concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123456789", "Nokia", "111111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Iphone iphone = new Iphone("987654321", "iPhone", "222222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
